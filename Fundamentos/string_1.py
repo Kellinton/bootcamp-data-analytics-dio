2736d47bb4621e5b24aa0cd12566fbf3c5056b9f
Kellinton <kellintonbrasil@gmail.com>
2024-04-02 08:39:10 -0300

    fudamentos: strings métodos: upper, lower, title, strip, center, join

---
+++ Fundamentos/string_1.py
@@ -0,0 +1,19 @@
+texto = 'Python'
+
+print(texto.upper()) # PYTHON
+print(texto.lower()) # python
+print(texto.title()) # Python
+
+print(texto.strip()) #Python remover espaços dos lados, lstrip para a esuqerda e rstrip para a direita
+# console.log(texto.trim()) remover espaços em javascript
+
+print(texto.center(40, "#")) #   ##Python## centralizar com 10 caracteres
+
+print(" . ".join(texto)) #P.y.t.h.o.n # Juntar com ponto
+
+menu = "Python"
+
+print("####" + menu + "####")
+print(menu.center(14))
+print(menu.center(14, "#"))
+print("-".join(menu))
